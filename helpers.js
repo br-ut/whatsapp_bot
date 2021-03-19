bb1aa1a71a69c835dc756d74908a0d2430def5d3
br-ut <42611524+br-ut@users.noreply.github.com>
2021-03-19 06:24:09 +0000

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *whatsapp_bot*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *whatsapp_bot*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *whatsapp_bot*:  ```" + msg + "```"
 }
 
 
